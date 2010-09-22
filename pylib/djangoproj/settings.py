cfe325de2591326194656ce018ca41605de619bc
Francis Irving <francis@mysociety.org>
2010-09-22 11:02:02 +0100

    Take settings file from wtf too

--- pylib/djangoproj/settings.py
+++ pylib/djangoproj/settings.py
@@ -1,4 +1,24 @@
-# Django settings for bins project.
+# Django settings for djangoproj project.
+
+# Some special mysociety preamble in order to get hold of our config
+# file conf/general
+import os
+import sys
+package_dir = os.path.abspath(os.path.split(__file__)[0])
+
+paths = (
+    os.path.normpath(package_dir + "/../../pylib"),
+    # os.path.normpath(package_dir + "/../../commonlib/pylib"),
+    )
+
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
+import mysociety.config
+mysociety.config.set_file(os.path.abspath(package_dir + "/../../conf/general"))
+
+# Now follows the normal Django stuff.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,23 +29,23 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME = mysociety.config.get('BINS_DB_NAME')
+DATABASE_USER = mysociety.config.get('BINS_DB_USER')
+DATABASE_PASSWORD = mysociety.config.get('BINS_DB_PASS')
+DATABASE_HOST = mysociety.config.get('BINS_DB_HOST')
+DATABASE_PORT = mysociety.config.get('BINS_DB_PORT')
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
 SITE_ID = 1
 
@@ -35,7 +55,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(package_dir, "../../web/media/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -48,7 +68,7 @@ MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '3ko%35w7!48fnaen+tt11i^1nonqna!ek#+#)2uis6&doyb#92'
+SECRET_KEY = mysociety.config.get('DJANGO_SECRET_KEY')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -63,12 +83,13 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'bins.urls'
+ROOT_URLCONF = 'djangoproj.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(package_dir, "templates"),
 )
 
 INSTALLED_APPS = (
@@ -76,4 +97,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.gis',
+    'djangoproj.waterfall',
 )
