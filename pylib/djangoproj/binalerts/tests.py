0ab72ec508083299eec8fba7f54ace72cdcb369e
Francis Irving <francis@mysociety.org>
2010-09-22 18:45:10 +0100

    A first test (which fails, obviously)

--- pylib/djangoproj/binalerts/tests.py
+++ pylib/djangoproj/binalerts/tests.py
@@ -1,23 +1,27 @@
 """
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
+Integration-style tests for binalerts. These tests think of things from the web
+frontend point of view. They are designed to make sure the application behaves
+as required to the user.
 """
 
 from django.test import TestCase
+from django.test import Client
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+class FrontPageTest(TestCase):
+    def test_frontpage_asks_for_postcode(self):
+        c = Client()
+        response = c.get('/')
 
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Enter your postcode')
+        self.assertEqual(response.template.name, 'frontpage')
+
+# Example doctest in case we need it later
 __test__ = {"doctest": """
 Another way to test that 1 + 1 is equal to 2.
 
 >>> 1 + 1 == 2
 True
 """}
+
 
