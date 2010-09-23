1bb6a58fdb29d50563932597a33b7bf5af8ad8df
Francis Irving <francis@mysociety.org>
2010-09-23 14:32:22 +0100

    Display basic form.

--- pylib/djangoproj/binalerts/tests.py
+++ pylib/djangoproj/binalerts/tests.py
@@ -4,6 +4,9 @@ frontend point of view. They are designed to make sure the application behaves
 as required to the user.
 """
 
+# Various tips on testing forms:
+# http://stackoverflow.com/questions/2257958/django-unit-testing-for-form-edit
+
 from django.test import TestCase
 from django.test import Client
 
@@ -14,9 +17,9 @@ class FrontPageTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Please enter your postcode')
+        self.assertContains(response, '<input type="text" name="postcode" id="id_postcode" />')
+        self.assertContains(response, '<input type="submit" value="Go" />')
         self.assertEqual(response.template.name, 'binalerts/frontpage.html')
-
-
 
 
 
--- pylib/djangoproj/binalerts/views.py
+++ pylib/djangoproj/binalerts/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+from binalerts.forms import LocationForm
+
 def frontpage(request):
-    return render_to_response('binalerts/frontpage.html', {})
+    if request.method == 'POST': # If the form has been submitted...
+        form = LocationForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            # Process the data in form.cleaned_data
+            # ...
+            return HttpResponseRedirect('/thanks/') # Redirect after POST
+    else:
+        form = LocationForm() # An unbound form
+
+    return render_to_response('binalerts/frontpage.html', { 'form': form })
 
 
