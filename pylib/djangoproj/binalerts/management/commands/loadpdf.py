eb404e62e289d87d55453e322749e07ea490ab46
Francis Irving <francis@mysociety.org>
2010-10-07 22:08:33 +0100

    Transaction didn't commit, just disable it for now.

--- pylib/djangoproj/binalerts/management/commands/loadpdf.py
+++ pylib/djangoproj/binalerts/management/commands/loadpdf.py
@@ -13,8 +13,8 @@ class Command(BaseCommand):
         if len(args) != 1:
             raise CommandError("Only argument is name of .xml file in fixtures directory to load, e.g. garden_sample_pdf.xml")
 
-        transaction.enter_transaction_management()
-        transaction.managed(True)
+        #transaction.enter_transaction_management()
+        #transaction.managed(True)
         
         file_to_load = os.path.join(os.path.dirname(binalerts.__file__), 'fixtures/', args[0])
         models.BinCollection.objects.load_from_pdf_xml(file_to_load)
